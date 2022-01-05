5f01070b6b056ffd2e7c71c982f487e6409720e4
Eduardo <dutomazoni@gmail.com>
2022-01-05 16:52:14 -0300

    fixed body size

--- main.js
+++ main.js
@@ -9,7 +9,8 @@ import path from "path";
 dotenv.config({ path: path.join(__dirname, '.env') });
 let app = express();
 app.use(cors());
-app.use(bodyParser.json());
+// app.use(bodyParser.json());
+app.use(bodyParser.json({limit: '50mb'}));
 app.use(routes);
 let port = process.env.PORT || 5001
 
@@ -20,7 +21,7 @@ app.use((req, res, next) => {
     next();
 });
 
-let mongoDb = process.env.URL
+let mongoDb =  process.env.MONGOURI || "mongodb+srv://animadex:animadex@cluster0.thb2w.mongodb.net/anima_dex?retryWrites=true&w=majority";
 
 mongoose.connect(mongoDb, { useNewUrlParser: true , useUnifiedTopology: true, useFindAndModify: false});
 let db = mongoose.connection
