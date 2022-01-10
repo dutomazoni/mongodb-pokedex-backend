64e87cc25ed314cd28a04abc1037214d9414c837
Eduardo <dutomazoni@gmail.com>
2022-01-10 12:32:57 -0300

    .env example

--- main.js
+++ main.js
@@ -9,7 +9,6 @@ import path from "path";
 dotenv.config({ path: path.join(__dirname, '.env') });
 let app = express();
 app.use(cors());
-// app.use(bodyParser.json());
 app.use(bodyParser.json({limit: '50mb'}));
 app.use(routes);
 let port = process.env.PORT || 5001
@@ -21,7 +20,7 @@ app.use((req, res, next) => {
     next();
 });
 
-let mongoDb =  process.env.MONGOURI || "mongodb+srv://animadex:animadex@cluster0.thb2w.mongodb.net/anima_dex?retryWrites=true&w=majority";
+let mongoDb =  process.env.MONGOURI;
 
 mongoose.connect(mongoDb, { useNewUrlParser: true , useUnifiedTopology: true, useFindAndModify: false});
 let db = mongoose.connection
